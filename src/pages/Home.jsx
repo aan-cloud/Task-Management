aan-cloud/Task-Management

commit 1ad771c4b1a426dba1d78d72aeb0bca1a462d80f

feat: card detail

src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
-import Features from "../component/features";
-import Main from "../component/main";
-import Form from "../component/form";
-import { Outlet } from "react-router-dom";
+import Features from "../component/Features";
+import Main from "../component/Main";
+import Form from "../component/Form";
+import Card from "../component/Card";
 
 export default function Home() {
   const [cards, setCards] = useState([]);
@@ -88,6 +88,7 @@ export default function Home() {
           of where your project
         </p>
       </section>
+      <Card />
       <Features openForm={handleForm} form={form}>
         <Form
           closeForm={handleCloseForm}
@@ -104,7 +105,6 @@ export default function Home() {
         onDeleteItem={handleDeleteItem}
         handleEdit={handleEdit}
       />
-      <Outlet />
     </>
   );
 }

src/component/Cards.jsx
@@ -1,0 +1,67 @@
+import { Link } from "react-router-dom";
+
+export default function Card({ card, handleEdit, onDeleteItem }) {
+  return (
+    <div className="px-[15px] py-[14px] border-[1px] rounded-md  bg-[#d8d8d8] border-[#b1b1b1] hover:scale-105 transition-all">
+      <h4 id="header" className="text-[18px] font-bold text-[#424242] ">
+        {card.title}
+      </h4>
+      <small id="date" className="text-[11px] text-[#656565] ">
+        {card.date}
+      </small>
+      <div id="status" className="flex items-center w-fit mt-[17px] mb-[25px]">
+        <div
+          id="color"
+          className="w-[12px] h-[15px] bg-red-500 rounded-tl-sm rounded-bl-sm"
+          style={
+            card.status === "on process"
+              ? { backgroundColor: "orange" }
+              : card.status === "done"
+              ? { backgroundColor: "green" }
+              : {}
+          }
+        ></div>
+        <div id="option" className=" text-[10px] px-[4px] bg-[#B1ABAB] ">
+          {card.status}
+        </div>
+      </div>
+      <div id="foot" className="flex items-center gap-2">
+        <button className="text-[12px] text-white bg-[#0075EA] font-normal hover:bg-[#2473c2] transition-all py-[7px] px-[10px] w-[80%] rounded-md ">
+          See Details
+        </button>
+        <Link to="/edit" className="p-[4px]" onClick={() => handleEdit(card)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-5 text-[#656565] hover:text-[#0075EA]"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+            />
+          </svg>
+        </Link>
+        <button onClick={() => onDeleteItem(card.id)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-5 text-[#656565] hover:text-[#0075EA]"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+}

src/component/card.jsx
@@ -1,65 +1,22 @@
-import { Link } from "react-router-dom";
-
-export default function Card({ card, handleEdit, onDeleteItem }) {
+export default function Card() {
   return (
-    <div className="px-[15px] py-[14px] border-[1px] rounded-md  bg-[#d8d8d8] border-[#b1b1b1] hover:scale-105 transition-all">
-      <h4 id="header" className="text-[18px] font-bold text-[#424242] ">
-        {card.title}
-      </h4>
-      <small id="date" className="text-[11px] text-[#656565] ">
-        {card.date}
-      </small>
-      <div id="status" className="flex items-center w-fit mt-[17px] mb-[25px]">
-        <div
-          id="color"
-          className="w-[12px] h-[15px] bg-red-500 rounded-tl-sm rounded-bl-sm"
-          style={
-            card.status === "on process"
-              ? { backgroundColor: "orange" }
-              : card.status === "done"
-              ? { backgroundColor: "green" }
-              : {}
-          }
-        ></div>
-        <div id="option" className=" text-[10px] px-[4px] bg-[#B1ABAB] ">
-          {card.status}
-        </div>
+    <div class="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+      <div class="p-6">
+        <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+          UI/UX Review Check
+        </h5>
+        <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
+          The place is close to Barceloneta Beach and bus stop just 2 min by
+          walk and near to "Naviglio" where you can enjoy the main night life in
+          Barcelona.
+        </p>
       </div>
-      <div id="foot" className="flex items-center gap-2">
-        <button className="text-[12px] text-white bg-[#0075EA] font-normal hover:bg-[#2473c2] transition-all py-[7px] px-[10px] w-[80%] rounded-md ">
-          See Details
-        </button>
-        <Link to="/edit" className="p-[4px]" onClick={() => handleEdit(card)}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-5 text-[#656565] hover:text-[#0075EA]"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-            />
-          </svg>
-        </Link>
-        <button onClick={() => onDeleteItem(card.id)}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-5 text-[#656565] hover:text-[#0075EA]"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-            />
-          </svg>
+      <div class="p-6 pt-0">
+        <button
+          class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+          type="button"
+        >
+          Read More
         </button>
       </div>
     </div>

src/component/main.jsx
@@ -1,4 +1,5 @@
-import Card from "./card";
+import Cards from "./Cards";
+
 export default function ({ cards, onDeleteItem, handleEdit }) {
   return (
     <section
@@ -6,7 +7,7 @@ export default function ({ cards, onDeleteItem, handleEdit }) {
       className="border-t-[2px] mt-[15px] px-[35px] py-[30px] grid grid-cols-auto-fill-minmax gap-5"
     >
       {cards.map((card, i) => (
-        <Card
+        <Cards
           key={i}
           card={card}
           handleEdit={handleEdit}

commit bd83f0c1360d9b0d5946334b7bc384107e9fa9cc

fix: Import issue
Introduced by 1ad771c4b1a426dba1d78d72aeb0bca1a462d80f

src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
-import Features from "../component/Features";
-import Main from "../component/Main";
-import Form from "../component/Form";
-import Card from "../component/Card";
+import Features from "../component/features";
+import Main from "../component/main";
+import Form from "../component/form";
+import Card from "../component/card";
 
 export default function Home() {
   const [cards, setCards] = useState([]);